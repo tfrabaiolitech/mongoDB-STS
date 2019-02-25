7090df3d1305783d0e419fe40bc15d297afa2d53
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-02-25 16:05:31 +0000

    DTO for return User

--- src/main/java/tech/tfrabaioli/workshopmongo/resources/UserResource.java
+++ src/main/java/tech/tfrabaioli/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package tech.tfrabaioli.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import tech.tfrabaioli.workshopmongo.domain.User;
+import tech.tfrabaioli.workshopmongo.dto.UserDTO;
 import tech.tfrabaioli.workshopmongo.services.UserService;
 
 @RestController
@@ -19,10 +21,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
- 	public ResponseEntity<List<User>> findAll() {
-		
+ 	public ResponseEntity<List<UserDTO>> findAll() {	
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
