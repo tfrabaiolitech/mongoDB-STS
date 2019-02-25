a9a3439f5582e144a79bb6feed2a00dff7de567b
tfrabaiolitech <tfrabaioli.tech@gmail.com>
2019-02-25 15:25:25 +0000

    Entity User and REST ok

---
+++ src/main/java/tech/tfrabaioli/workshopmongo/resources/UserResource.java
@@ -0,0 +1,27 @@
+package tech.tfrabaioli.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import tech.tfrabaioli.workshopmongo.domain.User;
+
+@RestController // Resource REST
+@RequestMapping(value="/users") //path end point 
+public class UserResource {
+
+	@RequestMapping(method=RequestMethod.GET) //method end point REST
+ 	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+	
+}
